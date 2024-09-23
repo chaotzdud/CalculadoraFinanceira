f0571f9b7dd61a24d2de3aa00a2d08a2c866f3da
dudz <eduarda.marcondes@fatec.sp.gov.br>
2024-09-23 20:15:20 -0300

    add button on main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:calculadora_de_juros/routes/routes.dart';
+import 'package:calculadora_de_juros/util/buttom.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,6 +31,16 @@ class TiposJuros extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Buttom('Juros Simples', Routes.call(context, '/result')),
+            Buttom('Juros Compostos', Routes.call(context, '/#'))
+          ],
+        )
+      )
     );
   }
 }
